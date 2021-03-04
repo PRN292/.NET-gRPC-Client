1b15e46e2e3b0b58ece486b3ea4ec644c66909e5
TienTruong2000 <tientt1938@gmail.com>
2021-03-04 14:53:47 +0700

    first commit

---
+++ TestGRPC/GRPCService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcImage;
+
+namespace TestGRPC
+{
+    class GRPCService
+    {
+        public string Address{ get; set; }
+        private GrpcChannel grpcChannel;
+        private ProcessImage.ProcessImageClient client;
+
+        public GRPCService(string Address)
+        {
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            this.Address = Address;
+            grpcChannel = GrpcChannel.ForAddress(Address);
+            client = new ProcessImage.ProcessImageClient(grpcChannel);
+        }
+
+        public async Task GetProcessedFrame()
+        {
+            using (var call = client.ProcessImage(new ProcessImageRequest { Id = "1" }))
+            {
+                var responseStream = call.ResponseStream;
+                int i = 0;
+                while (await responseStream.MoveNext())
+                {
+                    Console.WriteLine(i++);
+                    ProcessImageReply reply = responseStream.Current;
+                    Console.WriteLine(reply);
+                } 
+            }
+        }
+
+        public int DeleteEncoding(string image_name)
+        {
+            DeleteEncodingRequest request = new DeleteEncodingRequest { ImageName = image_name };
+            DeleteEncodingReply reply = client.DeleteEncoding(request);
+            return reply.Count;
+        }
+
+        public CreateEncodingReply CreateEncoding(string user_email, string image_b64_string)
+        {
+            CreateEncodingRequest request = new CreateEncodingRequest
+            {
+                Image = image_b64_string,
+                UserEmail = user_email
+            };
+            CreateEncodingReply reply = client.CreateEncoding(request);
+            return reply;
+        }
+    }
+}
---
+++ TestGRPC/Program.cs
@@ -0,0 +1,39 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using GrpcImage;
+using System.Runtime.InteropServices;
+using System.Net.Http;
+using Grpc.Core;
+
+namespace TestGRPC
+{
+
+    class Program
+    {
+        static public async Task Main(string[] args)
+        {
+            string serverAddr = "http://localhost:50051";
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress(serverAddr);
+            var client = new GrpcImage.ProcessImage.ProcessImageClient(channel);
+            using (var call = client.ProcessImage(new ProcessImageRequest { Id = "1" }))
+            {
+                var responseStream = call.ResponseStream;
+                int i = 0;
+                while (await responseStream.MoveNext())
+                {
+                    Console.WriteLine(i++);
+                    ProcessImageReply reply = responseStream.Current;
+                    Console.WriteLine(reply.Result);
+                }
+            }
+           /* DeleteEncodingReply reply = client.DeleteEncoding(
+                    new DeleteEncodingRequest { ImageName = "7bf140b7-bf44-4528-b445-c257535d51a2.png" }
+                );
+            Console.WriteLine("Count: " + reply.Count);*/
+        }
+    }
+}
